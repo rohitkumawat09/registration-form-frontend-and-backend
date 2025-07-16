c70b1db3e3df3329baad94bd6f461d79ce536471
Your Name <your-email@example.com>
2025-07-16 02:55:49 -0700

    first

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -1,12 +1,13 @@
 import DetailModel from "../models/user.js";
 
 export async function addData(req, res) {
+    console.log("first");
   try {
     console.log(req.body);
 
     const dataToAdd = new DetailModel(req.body);
     await dataToAdd.save();
-    res.status(200).send("Data Added");
+    res.status(200).send("Data Added",dataToAdd);
   } catch (error) {
     console.log(error);
     res.status(500).send("There is an error", error.message);
